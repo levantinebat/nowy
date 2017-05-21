58a57cddb5aa8b5965fd3a5387ccedf1ecccfb47
Denis Kostrzewa <denis@dkcomm.dk>
2017-05-21 18:02:29 +0200

    three

--- dato.config.js
+++ dato.config.js
@@ -4,24 +4,24 @@ const ellipsize = require('ellipsize');
 module.exports = (dato, root, i18n) => {
 
   // inside a "post" directory...
-  root.directory("content/episode", (dir) => {
-
-    // ...iterate over the "Blog post" records...
-    dato.blogPosts.forEach((blogPost) => {
-
-      // ...and create a markdown file for each article!
-      dir.createPost(`${blogPost.slug}.html`, "toml", {
-        frontmatter: {
-          title: blogPost.title,
-          type: "post",
-          date: blogPost.publishedAt,
-          category: blogPost.content,
-        },
-        content: blogPost.content
-      });
-    });
-  });
-  root.directory("content/about", (dir) => {
+  // root.directory("content/episode", (dir) => {
+  //
+  //   // ...iterate over the "Blog post" records...
+  //   dato.blogPosts.forEach((blogPost) => {
+  //
+  //     // ...and create a markdown file for each article!
+  //     dir.createPost(`${blogPost.slug}.html`, "toml", {
+  //       frontmatter: {
+  //         title: blogPost.title,
+  //         type: "post",
+  //         date: blogPost.publishedAt,
+  //         category: blogPost.content,
+  //       },
+  //       content: blogPost.content
+  //     });
+  //   });
+  // });
+  root.directory("content", (dir) => {
     const about = dato.aboutPage;
     dir.createPost("about.html", "toml", {
       frontmatter:  {
