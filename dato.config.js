4cd5de24ad860412bbf7e19266ae09cfc5140559
Denis Kostrzewa <denis@dkcomm.dk>
2017-05-21 17:54:15 +0200

    three

--- dato.config.js
+++ dato.config.js
@@ -19,6 +19,31 @@ module.exports = (dato, root, i18n) => {
         },
         content: blogPost.content
       });
-    }); 
+    });
   });
+  root.directory("content/about", (dir) => {
+    const about = dato.aboutPage;
+    dir.createPost("about.html", "toml", {
+      frontmatter:  {
+        historia: about.historia,
+        title: about.title,
+      },
+      content: about.content,
+    });
+
+
+    // ...iterate over the "Blog post" records...
+      // ...and create a markdown file for each article!
+      // dir.createPost(`${about_page.slug}.html`, "toml", {
+      //   frontmatter: {
+      //     title: about_page.title,
+      //     historia: about_page.historia,
+      //     type: "post",
+      //     date: about_page.publishedAt,
+      //   },
+      //   content: about_page.content
+      // });
+
+  });
+
 }
